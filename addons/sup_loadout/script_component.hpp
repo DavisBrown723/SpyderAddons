6c540bdf51a24564fcc214fca3e32c52018dbe6f
SpyderBlack723 <spyderblack8@gmail.com>
2015-11-24 16:23:43 -0600

    [All] Added CBA Macros

--- addons/sup_loadout/script_component.hpp
+++ addons/sup_loadout/script_component.hpp
@@ -1,2 +1,4 @@
 #define COMPONENT sup_loadout
 #include <\x\spyderaddons\addons\main\script_mod.hpp>
+
+#include <\x\cba\addons\main\script_macros.hpp>
--- addons/x_lib/script_component.hpp
+++ addons/x_lib/script_component.hpp
@@ -1,2 +1,4 @@
 #define COMPONENT x_lib
 #include <\x\spyderaddons\addons\main\script_mod.hpp>
+
+#include <\x\cba\addons\main\script_macros.hpp>
