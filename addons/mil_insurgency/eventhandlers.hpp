19ec4fca72f0952064cc0a74fa4ee6ac2dc6e54c
SpyderBlack723 <spyderblack8@gmail.com>
2015-11-24 16:21:40 -0600

    Removed Files

--- addons/mil_insurgency/eventhandlers.hpp
+++
@@ -1,7 +0,0 @@
-class Extended_Init_EventHandlers {
-	class All {
-		class COMPONENT {
-			init = "_this call SpyderAddons_fnc_addMapAction";
-		};
-	};
-};
--- addons/mil_insurgency/fnc_addMapAction.sqf
+++
@@ -1,5 +0,0 @@
-params ["_object"];
-
-if (typeOf _object == "Land_MapBoard_F") then {
-	_object addAction ["View Map", {hint "Yes, this does work"}];
-};
