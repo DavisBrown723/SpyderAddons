50e73ade5bfb022673e78d3404fc19b1a3f5c3af
SpyderBlack723 <spyderblack8@gmail.com>
2015-12-21 12:32:47 -0600

    [civ_interact] Removed ALiVE dependencies to prevent A3 error messages if ALiVE isn't running

--- addons/civ_interact/CfgPatches.hpp
+++ addons/civ_interact/CfgPatches.hpp
@@ -3,7 +3,7 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 1.50;
-		requiredAddons[] = {"A3_Modules_F","cba_xeh_a3","ALIVE_main"};
+		requiredAddons[] = {"A3_Modules_F","cba_xeh_a3"};
 		author[] = {"SpyderBlack723"};
 	};
 };
