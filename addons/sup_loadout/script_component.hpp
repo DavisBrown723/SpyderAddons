5c9162d40adcb0e2c361739e6a7d23ba9e0fe580
SpyderBlack723 <spyderblack8@gmail.com>
2016-04-06 18:25:11 -0500

    [sup_loadout] Updated script_component to new macro system

--- addons/sup_loadout/script_component.hpp
+++ addons/sup_loadout/script_component.hpp
@@ -1,4 +1,4 @@
 #define COMPONENT sup_loadout
 #include <\x\spyderaddons\addons\main\script_mod.hpp>
 
-#include <\x\cba\addons\main\script_macros.hpp>
+#include <\x\spyderaddons\addons\main\script_macros.hpp>
